12288778f72c05e41f2fc9bd3f5252ae32ded8a2
Marcio Coelho <marciocoelho@gmail.com>
2021-08-29 14:27:03 -0300

    Create about.component.*

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from "@angular/core";
+
+import { AppComponent } from "../app.component";
+
+@Component({
+  selector:    "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls:   [
+    "./about.component.scss",
+    "../app.component.scss"
+  ]
+})
+export class AboutComponent implements OnInit {
+  appComponent = new AppComponent();
+  name = "Marcio Coelho";
+  ra = "0050831921015";
+  githubPage = "https://coelhomarcio.github.io/";
+  githubRepo = "https://github.com/coelhomarcio/";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
